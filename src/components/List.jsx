ac4b70d1a36460e3ec0555693f4e60317021293d
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-01-09 13:55:16 -0700

    Commented

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { useState } from 'react';
 
 const List = () => {
+    // array of names
     const [list, setList] = useState(['Murphy', 'Rigney', 'Noah', 'Ethan', 'Ren', 'Andres', 'Carillo', 'Jon', 'Mercutio', 'Kyle'])
+    // sets type of list
     const [type, setType] = useState('ul')
+    // sets direction of the list
     const [direction, setDirection] = useState('horizontal')
-    console.log(list)
+    // if the list is ordered, return it with the <ol> element
+    // classname lines up with a css class that determines the direction of the list
     if(type == 'ol'){
         return (
             <ol className={direction}>
@@ -18,6 +22,7 @@ const List = () => {
             </ol>
         )
     }
+    // if the list isn't ordered, or if it is undefined, it will return an unordered list, which means it is the default list state
     return (
         <ul className={direction}>
             {list.map(item=>{
